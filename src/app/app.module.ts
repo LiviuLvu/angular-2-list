290c058089f0952fd7e86f2bb091f6fd25637235
Liviu Iancu <liviuiancu@gmx.com>
2017-06-07 22:15:28 +0300

    enabled tracing on appRoutes, added paths for 404 and home redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,30 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'directory', component: DirectoryComponent },
+  { path: '', 
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DirectoryComponent
+    DirectoryComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   exports: [RouterModule],
   providers: [],
