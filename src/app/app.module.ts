136ad51999e5665d8fb8abefbe47c524657b5073
Liviu Iancu <liviuiancu@gmx.com>
2017-06-07 23:55:08 +0300

    imported modules to experiment with http and jsonp calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,20 +20,20 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+  ],
   declarations: [
     AppComponent,
     HomeComponent,
     DirectoryComponent,
     PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true })
-  ],
   exports: [RouterModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
